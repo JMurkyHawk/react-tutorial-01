db797bd30734b4aa2e350f993eb5e93d45a32ca4
JMurkyHawk <jmiaguard-github@yahoo.com>
2024-02-02 06:48:00 -0600

    Tutorial project updates - fetch data from api in react

--- src/App.js
+++ src/App.js
@@ -6,13 +6,36 @@ import Footer from './Footer';
 import { useState, useEffect } from 'react';
 
 function App() {
-    const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')) || []);
+    const API_URL = 'http://localhost:3500/items';
+
+    const [items, setItems] = useState([]);
     const [newItem, setNewItem] = useState('');
     const [search, setSearch] = useState('');
+    const [fetchError, setFetchError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        localStorage.setItem('shoppingList', JSON.stringify(items));
-    }, [items]);
+        const fetchItems = async () => {
+            try {
+                const response = await fetch(API_URL);
+                if (!response.ok) throw Error('Did not receive expected data');
+                const listItems = await response.json();
+                setItems(listItems);
+                setFetchError(null);
+            } catch (err) {
+                setFetchError(err.message);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        setTimeout(() => { // setTimeout simulates call from server
+            // fetchItems does not return a value. Therefore, this async IIFE 
+            // (instantly invoked function expression) is not required.
+            // You can just make a call to fetchItems();
+            fetchItems();
+            //(async () => await fetchItems())();
+        }, 2000);
+    }, []);
 
     const addItem = (item) => {
         const id = items.length ? items[items.length - 1].id + 1 : 1;
@@ -54,11 +77,15 @@ function App() {
                 search={search} 
                 setSearch={setSearch} 
             />
-            <Content 
-                items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))} 
-                handleCheck={handleCheck} 
-                handleDelete={handleDelete} 
-            />
+            <main>
+                {isLoading && <p>Loading Items...</p>}
+                {fetchError && <p style={{color: 'red'}}>{`Error: ${fetchError}`}</p>}
+                {!fetchError && !isLoading && <Content 
+                    items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))} 
+                    handleCheck={handleCheck} 
+                    handleDelete={handleDelete} 
+                />}
+            </main>
             <Footer length={items.length} />
         </div>
     );
